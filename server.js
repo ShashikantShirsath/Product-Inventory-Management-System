10445bf04e985b2a01499fd47bad69ff937994d1
ShashikantShirsath <shashikantshirsath08@gmail.com>
2026-02-18 22:21:29 +0530

    feature: setup database connection

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
