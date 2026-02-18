d42aa9992ffc376e515aed09538f2efcf41263bb
ShashikantShirsath <shashikantshirsath08@gmail.com>
2026-02-18 22:02:32 +0530

    feature: setup initial project, created folder structure and server

---
+++ server.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+})
